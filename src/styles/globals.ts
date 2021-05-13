a80a88c23c3b9f86d9d74c00ab084ba97c47caf6
Victor <bastosleft@gmail.com>
2021-05-13 16:24:56 -0300

    feat: Criação do estilo global da aplicação

---
+++ src/styles/globals.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+  
+    body{
+        -webkit-font-smoothing: antialiased;
+    }
+    body, input, textarea, button{
+        font-family: 'Source Sans Pro', sans-serif;
+        font-weight: 400;
+    }
+    h1,h2,h3,h4,h5,h6, strong{
+        font-weight: 600;
+    }
+    button{
+        cursor: pointer;
+    }
+`
+
