9552f04ee0dcc9e59f676875e58ba358945d43f5
Victor <bastosleft@gmail.com>
2021-05-13 16:26:45 -0300

    feat: Criação de objeto de cores

---
+++ src/utils/Colors.ts
@@ -0,0 +1,19 @@
+export const Colors = {
+    grayDark: "#2F3136",
+    gray: "#36393F",
+    grayButton: "#474C53",
+    grayLight: "#72767D",
+    red: "#ED4245",
+    blue: "#7289DA",
+    green: "#3BA55C",
+    text: "#DCDDDE",
+    white: "#FFFFFF",
+    title: "#8E9297",
+}
+
+
+
+
+
+
+
