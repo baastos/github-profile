33b6b313087d64210a3f21e2fd03685d11cc9ef5
Victor <bastosleft@gmail.com>
2021-05-15 17:04:45 -0300

    refactory: envio da function getToken para o contexto

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,56 +4,21 @@ import LogoImg from '../../assets/logo.svg'
 import { Button } from '../../components/Button'
 import { Footer } from '../../components/Footer'
 import { useEffect } from 'react'
-import { api } from '../../services/api'
 import { useAuth } from '../../hooks/useAuth'
 
-
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-const SCOPES = 'identify%20email'
+const SCOPES = 'identify'
 const authUri = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPES}`
 
+
 export function Login() {
-    const { updateUser, isUserAuthenticated } = useAuth();
+    const { getToken } = useAuth();
 
     useEffect(() => {
-
-        async function getUser() {
-            const urlParams = new URLSearchParams(window.location.search)
-            const code = urlParams.get('code');
-
-            if (code) {
-                const body = "grant_type=authorization_code"
-                    + `&client_id=${CLIENT_ID}`
-                    + `&client_secret=${CLIENT_SECRET}`
-                    + `&code=${code}`
-                    + `&redirect_uri=${REDIRECT_URI}`
-
-                const response = await api.post('oauth2/token', body, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    }
-                })
-
-                const { access_token: token } = response.data;
-
-                const user = await api.get('users/@me', {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                })
-
-                isUserAuthenticated(true);
-
-                updateUser(user.data)
-
-            }
-        }
-        getUser();
+        getToken();
 
     })
-
 
     return (
         <LoginContainer>
