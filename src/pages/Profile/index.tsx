b46f96b4254686bd2921b7b1806e945b3fb42eec
Victor <bastosleft@gmail.com>
2021-05-14 23:04:32 -0300

    feat: Criação layout do aside

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,12 +1,53 @@
+import { Button } from "../../components/Button";
 import { useAuth } from "../../hooks/useAuth"
+import { Colors } from "../../utils/Colors";
+import { AsideContainer, FriendListContainer, InputSearch, ProfileContainer } from "./styles"
+
+const DEFAULT_AVATAR_URL = 'http://i.imgur.com/EroY8Ii.png'
 
 export function Profile(): JSX.Element {
-    const { logoutUser, user } = useAuth()
+    const { logoutUser, user } = useAuth();
+
     return (
-        <div>
-            <h1>PROFILE</h1>
-            <button onClick={logoutUser}>Sign Out</button>
-            <p>{JSON.stringify(user)}</p>
-        </div>
+        <ProfileContainer>
+            <AsideContainer>
+                <InputSearch placeholder="Encontre um amigo" />
+                <FriendListContainer>
+                    <strong>Meus amigos</strong>
+                    <ul>
+                        <li>
+                            <div>
+                                <img src={DEFAULT_AVATAR_URL} alt={DEFAULT_AVATAR_URL} />
+                                <div>
+                                    <strong>
+                                        Victor
+                                        <span>Jogando Visual Studio Code</span>
+                                    </strong>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div>
+                                <img src={DEFAULT_AVATAR_URL} alt={DEFAULT_AVATAR_URL} />
+                                <div>
+                                    <strong>NatanT</strong>
+                                </div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </FriendListContainer>
+
+
+                {/* PROVISÓRIO */}
+                <div style={{ alignSelf: "center" }}>
+
+                    <Button onClick={logoutUser} style={{ background: Colors.red }}>Sair</Button>
+                </div>
+                {/* PROVISÓRIO */}
+
+            </AsideContainer>
+        </ProfileContainer>
     )
 }
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,4 +1,90 @@
 import styled from 'styled-components'
+import { Colors } from '../../utils/Colors';
 
 
-export const ProfileContainer = styled.div``;
+export const ProfileContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: ${Colors.gray};
+`;
+export const AsideContainer = styled.aside`
+  max-width: 312px;
+  height: 100vh;
+  background: ${Colors.grayDark};
+
+  display: flex;
+  flex-direction: column;
+
+`
+export const InputSearch = styled.input`
+  margin: 11px;
+  padding: 6px;
+  border: 2px solid ${Colors.gray};
+  outline: none;
+  background: ${Colors.gray};
+  color: ${Colors.text};
+  border-radius: 3px;
+
+  &::placeholder{
+    color: ${Colors.text};
+    font-weight:600
+  }
+  &:focus{
+  border: 2px solid ${Colors.blue};
+
+  }
+
+
+`
+export const FriendListContainer = styled.div`
+  padding: 18px 0px;
+  border-top: 2px solid ${Colors.gray};
+
+  strong{
+    
+    margin-left: 16px;
+    color: ${Colors.text};
+  }
+
+  ul{
+
+    padding: 16px 22px;
+    list-style: none;
+
+    li{
+
+      div{
+        display:flex;
+        align-items: center;
+        
+          img{
+            width:32px;
+            height:32px;
+            border-radius:50%;
+
+          }
+          div{
+            display: flex;
+            flex-direction:column;
+            margin-left:12px;
+
+            strong{
+              display: flex;
+              flex-direction: column;
+              font-size: 14px;
+              margin-left: 0;
+              color: ${Colors.text};
+            }
+            span{
+              font-size:12px;
+              color: ${Colors.text};
+            }
+          }
+      }
+      & + li{
+        margin-top: 12px;
+      }
+    }
+  }
+`
+
